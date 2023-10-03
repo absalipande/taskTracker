9f51dbe85d9c1d6a39aad4e1494c5f7a0c70e357
Amiel Brencis Salipande <absalipande@gmail.com>
2023-10-03 15:20:06 +0800

    style: Styling for Column and Board components

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,25 @@
-import Column from "./Column";
+import useDarkModeStore from '../store/DarkModeStore';
+import Column from './Column';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Board = () => {
-    return ( 
-        <Column id={'To-do'} tasks={[]} index={0}/>
-     );
-}
- 
+  const isDark = useDarkModeStore((state) => state.isDark);
+
+  return (
+    <div className={isDark ? 'bg-[#1A2020C]' : 'bg-[#EDF2F7]'}>
+      <div>
+        <DragDropContext onDragEnd={() => {}}>
+          <Droppable droppableId='board' direction='horizontal' type='column'>
+            {(provided) => (
+              <div {...provided.droppableProps} ref={provided.innerRef} className='grid grid-cols-1 lg:grid-cols-3 gap-4 p-4 max-w-[1400px] mx-auto'>
+                <Column id={'To-do'} tasks={[]} index={0} />
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    </div>
+  );
+};
+
 export default Board;
