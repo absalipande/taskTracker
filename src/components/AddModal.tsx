5da9e2be343ba5264a49a3a6a1f82df8f40d455d
Amiel Brencis Salipande <absalipande@gmail.com>
2023-10-03 17:11:51 +0800

    chore: Removed shadcn-ui

--- src/components/AddModal.tsx
+++ src/components/AddModal.tsx
@@ -1,7 +1,36 @@
+import { FormEvent } from 'react';
+import useDarkModeStore from '../store/DarkModeStore';
+import { useModalStore } from '../store/ModalStore';
+import { useBoardStore } from '../store/BoardStore';
+
 const AddModal = () => {
-    return ( 
-        <div></div>
-     );
-}
- 
+  const isDark = useDarkModeStore((state) => state.isDark);
+  const [isAddModalOpen, addModalClose, modalCategoryTitle] = useModalStore((state) => [
+    state.isAddModelOpen,
+    state.addModalClose,
+    state.modalCategoryTitle,
+    state.setModalCategoryTitle,
+  ]);
+  const [addTask, addTaskInput, setAddTaskInput] = useBoardStore((state) => [state.addTask, state.addTaskInput, state.setAddTaskInput]);
+
+  let title = '';
+  if (modalCategoryTitle === 'To-do') {
+    title = 'Add a new to-do';
+  } else if (modalCategoryTitle === 'In-progress') {
+    title = 'Add In-progress';
+  } else if (modalCategoryTitle === 'Done') {
+    title = 'Add Done';
+  }
+
+  const handleAddTask = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!addTaskInput) return;
+    addTask(addTaskInput, modalCategoryTitle);
+    setAddTaskInput('');
+    addModalClose();
+  };
+
+  return <div></div>;
+};
+
 export default AddModal;
